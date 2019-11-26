bb0ccf3824e9aaec415acb68c71682a5446180fc
czl960612 <18513348915@163.com>
2019-11-26 19:04:31 +0800

    添加请求封装

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import api from './http/index'
+
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(api)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
